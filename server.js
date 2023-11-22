52c7f473709d06fbb27c457456783b0a5cefdff2
lugasanegah <lugas@analisa.io>
2023-11-22 12:29:56 +0700

    Add | handle service

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3080;
 
 // Menyajikan berkas statis seperti index.html
 app.use(express.static(path.join(__dirname, 'public')));
